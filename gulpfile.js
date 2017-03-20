27870f14c9db5beeeb1ed8b5b67bdfcf3e0827a4
Patrick Artounian <partounian@gmail.com>
2017-03-19 17:13:56 -0700

    Add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
-var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
 
-gulp.task('scripts', function() {
+gulp.task('serve', ['js', 'styles'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("index.html").on('change', browserSync.reload);
+    gulp.watch("js/scripts.js", ['js-watch']);
+    gulp.watch("scss/styles.scss", ['styles']);
+});
+
+gulp.task('js', function() {
     return gulp.src('js/scripts.js')
         .pipe(plumber(plumber({
             errorHandler: function (err) {
@@ -18,6 +30,11 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('js'));
 });
 
+gulp.task('js-watch', ['js'], function(done) {
+    browserSync.reload();
+    done();
+})
+
 gulp.task('styles', function() {
     return gulp.src('scss/styles.scss')
         .pipe(plumber(plumber({
@@ -28,10 +45,13 @@ gulp.task('styles', function() {
         })))
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(autoprefixer())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', ['scripts', 'styles'], function() {
-    gulp.watch('js/*.js', ['scripts']);
+gulp.task('watch', ['js', 'styles'], function() {
+    gulp.watch('js/*.js', ['js']);
     gulp.watch('scss/*.scss', ['styles']);
 });
+
+gulp.task('default', ['serve']);
